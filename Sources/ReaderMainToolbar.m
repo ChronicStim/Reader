d0052095ac4fed1359b71e287a9ec8f493a3ad78
Development <support@chronicstimulation.com>
2016-04-21 10:22:04 -0400

    Update code per Xcode warnings

--- Sources/ReaderMainToolbar.m
+++ Sources/ReaderMainToolbar.m
@@ -91,7 +91,8 @@
 
 		UIFont *doneButtonFont = [UIFont systemFontOfSize:BUTTON_FONT_SIZE];
 		NSString *doneButtonText = NSLocalizedString(@"Done", @"button");
-		CGSize doneButtonSize = [doneButtonText sizeWithFont:doneButtonFont];
+        NSDictionary *textAttributes = @{doneButtonFont:NSFontAttributeName};
+        CGSize doneButtonSize = [doneButtonText sizeWithAttributes:textAttributes];
 		CGFloat doneButtonWidth = (doneButtonSize.width + TEXT_BUTTON_PADDING);
 
 		UIButton *doneButton = [UIButton buttonWithType:UIButtonTypeCustom];
